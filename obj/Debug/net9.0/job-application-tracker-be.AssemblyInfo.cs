87803387f59cb6ffaece8eb279de87ec87b61b01
Nm-Tan <asdwzzz99@gmail.com>
2026-04-17 18:54:41 +0700

    update

--- obj/Debug/net9.0/job-application-tracker-be.AssemblyInfo.cs
+++ obj/Debug/net9.0/job-application-tracker-be.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("job-application-tracker-be")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e513279cbc4311b42b54669b4e152e682fef3aca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb7d5b3de5e56835ba1498c5ac319fbfcb15dda")]
 [assembly: System.Reflection.AssemblyProductAttribute("job-application-tracker-be")]
 [assembly: System.Reflection.AssemblyTitleAttribute("job-application-tracker-be")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
